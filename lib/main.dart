45e4210020f9f4ad81567113cee0a97aaf6626c4
PrateekMarke <markepratik@gmail.com>
2026-01-15 16:38:38 +0530

    firebase intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:student_expense_analyzer/core/constants/keys.dart';
 import 'package:student_expense_analyzer/config/route/app_router.dart';
 import 'package:student_expense_analyzer/config/theme/app_theme.dart';
+import 'package:student_expense_analyzer/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
